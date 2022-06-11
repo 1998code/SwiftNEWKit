f1773a0e1ff4fdeb69edbff1b2399d3900ff55b4
Ming <54872601+1998code@users.noreply.github.com>
2022-06-11 16:44:33 +0800

    Replace Incorrect File

--- Sources/SwiftNEW/SwiftNEW.swift
+++ Sources/SwiftNEW/SwiftNEW.swift
@@ -1,48 +1,130 @@
 import SwiftUI
  
-@available(iOS 14, macOS 11.0, *)
-public struct AnimatedMenuBar: View {
-    @Binding var selectedIndex: Int
-    @Namespace private var menuItemTransition
- 
-    var menuItems = [ "Travel", "Nature", "Architecture" ]
- 
-    public init(selectedIndex: Binding<Int>, menuItems: [String] = [ "Travel", "Nature", "Architecture" ]) {
-        self._selectedIndex = selectedIndex
-        self.menuItems = menuItems
+@available(iOS 14, watchOS 7.0, macOS 11.0, *)
+public struct SwiftNEW: View {
+    @AppStorage("version") var version = 1.0
+    @AppStorage("build") var build: Double = 1
+    @State var show = false
+    @State var items: [Model] = []
+    @State var align: HorizontalAlignment = .center
+    @State var loading = true
+    
+    public init() {
     }
  
     public var body: some View {
- 
-        HStack {
-            Spacer()
- 
-            ForEach(menuItems.indices) { index in
- 
-                if index == selectedIndex {
-                    Text(menuItems[index])
-                        .padding(.horizontal)
-                        .padding(.vertical, 4)
-                        .background(Capsule().foregroundColor(Color.purple))
-                        .foregroundColor(.white)
-                        .matchedGeometryEffect(id: "menuItem", in: menuItemTransition)
-                } else {
-                    Text(menuItems[index])
-                        .padding(.horizontal)
-                        .padding(.vertical, 4)
-                        .background(Capsule().foregroundColor(Color( red: 244, green: 244, blue: 244)))
-                        .onTapGesture {
-                            selectedIndex = index
-                        }
-                }
- 
-                Spacer()
-            }
- 
+        Button(action: { show = true }) {
+            Text("Show Release Note")
+                .frame(width: 300, height: 50)
+                .foregroundColor(.white)
+                .background(Color.accentColor)
+                .cornerRadius(15)
         }
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .padding()
-        .animation(.easeInOut, value: selectedIndex)
- 
+        .sheet(isPresented: $show) {
+            VStack(alignment: align) {
+                Spacer()
+                Text("What's New?").bold().font(.largeTitle)
+                Text("Version \(UIApplication.versionBuild)")
+                    .bold().font(.title).foregroundColor(.secondary)
+                Spacer()
+                if loading {
+                    ProgressView()
+                } else {
+                    ScrollView {
+                        ForEach(items, id: \.self) { item in
+                            HStack {
+                                Image(systemName: item.icon)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Color.accentColor)
+                                    .frame(width: 50, height:50)
+                                    .cornerRadius(15)
+                                    .padding(.trailing)
+                                VStack(alignment: .leading) {
+                                    Text(item.title).font(.headline)
+                                    Text(item.subtitle).font(.subheadline).foregroundColor(.secondary)
+                                    Text(item.body).font(.caption).foregroundColor(.secondary)
+                                }
+                                Spacer()
+                            }
+                        }.frame(width: 300)
+                    }.frame(height: 450)
+                }
+                Spacer()
+                Button(action: { show = false }) {
+                    Text("Continue").bold()
+                }.frame(width: 300, height: 50)
+                .foregroundColor(.white)
+                .background(Color.accentColor)
+                .cornerRadius(15)
+            }
+            .onAppear {
+                compareVersion()
+                loadData()
+            }
+        }
     }
+
+    public func compareVersion() {
+        #if os(iOS)
+        if Double(UIApplication.version)! > version || Double(UIApplication.build)! > build {
+            show = true
+            version = Double(UIApplication.version)!
+            build = Double(UIApplication.build)!
+        }
+        #elseif os(macOS)
+        if Double(NSApplication.version)! > version || Double(NSApplication.build)! > build {
+            show = true
+            version = Double(UIApplication.version)!
+            build = Double(UIApplication.build)!
+        }
+        #endif
+    }
+    public func loadData() {
+        // MARK :- Local Data
+        if let url = Bundle.main.url(forResource: "data", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                items = try decoder.decode([Model].self, from: data)
+                loading = false
+            } catch {
+                print("error: \(error)")
+            }
+        }
+    }
+}
+
+// MARK: - Find Version
+#if os(iOS)
+public extension UIApplication {
+    static var version: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String? ?? "1.0"
+    }
+    static var build: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String? ?? "1"
+    }
+    static var versionBuild: String {
+        return "\(version) (\(build))"
+    }
+}
+#elseif os(macOS)
+public extension NSApplication {
+    static var version: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String? ?? "1.0"
+    }
+    static var build: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String? ?? "1"
+    }
+    static var versionBuild: String {
+        return "\(version) (\(build))"
+    }
+}
+#endif
+
+public struct Model: Codable, Hashable {
+    var icon: String
+    var title: String
+    var subtitle: String
+    var body: String
 }
