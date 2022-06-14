cd5dd991a768dd9e51b108385fb76151785a41c1
Ming <54872601+1998code@users.noreply.github.com>
2022-06-15 02:10:14 +0800

    Test

--- Package.swift
+++ Package.swift
@@ -8,11 +8,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftNEW",
-    platforms: [
-        .iOS(.v14),
-        .watchOS(.v7),
-        .macOS(.v11)
-    ],
     products: [
         .library(
             name: "SwiftNEW",
