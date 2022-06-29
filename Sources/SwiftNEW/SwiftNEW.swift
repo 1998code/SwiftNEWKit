aa1943e7801daa7d55fe346fb5cc03e859add705
Ming <54872601+1998code@users.noreply.github.com>
2022-06-29 10:54:26 +0800

    Change textColor to labelColor

--- Sources/SwiftNEW/SwiftNEW.swift
+++ Sources/SwiftNEW/SwiftNEW.swift
@@ -16,16 +16,16 @@ public struct SwiftNEW: View {
     @Binding var show: Bool
     @Binding var align: HorizontalAlignment
     @Binding var color: Color
-    @Binding var textColor: Color
     @Binding var size: String
+    @Binding var labelColor: Color
     @Binding var label: String
     @Binding var labelImage: String
 
-    public init( show: Binding<Bool>, align: Binding<HorizontalAlignment>, color: Binding<Color>, textColor: Binding<Color>, size: Binding<String>, label: Binding<String>, labelImage: Binding<String>) {
+    public init( show: Binding<Bool>, align: Binding<HorizontalAlignment>, color: Binding<Color>, labelColor: Binding<Color>, size: Binding<String>, label: Binding<String>, labelImage: Binding<String>) {
         _show = show
         _align = align
         _color = color
-        _textColor = textColor
+        _labelColor = labelColor
         _size = size
         _label = label
         _labelImage = labelImage
@@ -41,7 +41,7 @@ public struct SwiftNEW: View {
                 Label(label, systemImage: labelImage)
                     .frame(width: 300, height: 50)
                     #if os(iOS)
-                    .foregroundColor(textColor)
+                    .foregroundColor(labelColor)
                     .background(color)
                     .cornerRadius(15)
                     #endif
