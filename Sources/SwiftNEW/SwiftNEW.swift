9a1bcef185f65039f57135d4f8623a8246d66cb0
Ming <54872601+1998code@users.noreply.github.com>
2022-07-10 10:53:43 +0800

    Update demo

--- Sources/SwiftNEW/SwiftNEW.swift
+++ Sources/SwiftNEW/SwiftNEW.swift
@@ -20,6 +20,8 @@ public struct SwiftNEW: View {
     @Binding var labelColor: Color
     @Binding var label: String
     @Binding var labelImage: String
+    
+    @State var showHistory: Bool = false
 
     public init( show: Binding<Bool>, align: Binding<HorizontalAlignment>, color: Binding<Color>, size: Binding<String>, labelColor: Binding<Color>, label: Binding<String>, labelImage: Binding<String>) {
         _show = show
@@ -48,59 +50,100 @@ public struct SwiftNEW: View {
             }
         }
         .sheet(isPresented: $show) {
-            VStack(alignment: align) {
-                Spacer()
-                Text("What's New?").bold().font(.largeTitle)
-                Text("Version \(Bundle.versionBuild)")
-                    .bold().font(.title).foregroundColor(.secondary)
-                Spacer()
-                if loading {
-                    ProgressView()
-                } else {
-                    ScrollView {
-                        ForEach(items, id: \.self) { item in
-                            if item.version == Bundle.version {
-                                ForEach(item.new, id: \.self) { new in
-                                    HStack {
-                                        ZStack {
-                                            color
-                                            Image(systemName: new.icon)
-                                                .foregroundColor(.white)
-                                        }
-                                        .frame(width: 50, height:50)
-                                        .cornerRadius(15)
-                                        .padding(.trailing)
-                                        VStack(alignment: .leading) {
-                                            Text(new.title).font(.headline).lineLimit(1)
-                                            Text(new.subtitle).font(.subheadline).foregroundColor(.secondary).lineLimit(1)
-                                            Text(new.body).font(.caption).foregroundColor(.secondary).lineLimit(2)
-                                        }
-                                        Spacer()
-                                    }.padding(.bottom)
-                                }
+            sheetContent
+        }
+        .sheet(isPresented: $showHistory) {
+            sheetHistory
+        }
+    }
+    
+    public var sheetContent: some View {
+        VStack(alignment: align) {
+            Spacer()
+            Text("What's New?").bold().font(.largeTitle)
+            Text("Version \(Bundle.versionBuild)")
+                .bold().font(.title).foregroundColor(.secondary)
+            Spacer()
+            if loading {
+                ProgressView()
+            } else {
+                ScrollView {
+                    ForEach(items, id: \.self) { item in
+                        if item.version == Bundle.version {
+                            ForEach(item.new, id: \.self) { new in
+                                HStack {
+                                    ZStack {
+                                        color
+                                        Image(systemName: new.icon)
+                                            .foregroundColor(.white)
+                                    }
+                                    .frame(width: 50, height:50)
+                                    .cornerRadius(15)
+                                    .padding(.trailing)
+                                    VStack(alignment: .leading) {
+                                        Text(new.title).font(.headline).lineLimit(1)
+                                        Text(new.subtitle).font(.subheadline).foregroundColor(.secondary).lineLimit(1)
+                                        Text(new.body).font(.caption).foregroundColor(.secondary).lineLimit(2)
+                                    }
+                                    Spacer()
+                                }.padding(.bottom)
                             }
                         }
-                    }.frame(width: 300, height: 450)
+                    }
+                }.frame(width: 300, height: 450)
+            }
+            Spacer()
+            Button(action: { showHistory = true }) {
+                HStack {
+                    Text("Show History")
+                    Image(systemName: "arrow.up.bin")
                 }
-                Spacer()
-                Button(action: { show = false }) {
+            }
+            Button(action: { show = false }) {
+                HStack{
                     Text("Continue").bold()
-                    .frame(width: 300, height: 50)
-                    #if os(iOS)
-                    .foregroundColor(.white)
-                    .background(color)
-                    .cornerRadius(15)
-                    #endif
+                    Image(systemName: "arrow.right.circle.fill")
                 }
-                .padding(.bottom)
+                .frame(width: 300, height: 50)
+                #if os(iOS)
+                .foregroundColor(.white)
+                .background(color)
+                .cornerRadius(15)
+                #endif
             }
-            .onAppear {
-                loadData()
+            .padding(.bottom)
+        }
+        .onAppear {
+            loadData()
+        }
+        #if os(macOS)
+        .padding()
+        .frame(width: 600, height: 600)
+        #endif
+    }
+    
+    public var sheetHistory: some View {
+        List {
+            ForEach(items, id: \.self) { item in
+                ForEach(item.new, id: \.self) { new in
+                    HStack {
+                        ZStack {
+                            color
+                            Image(systemName: new.icon)
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 50, height:50)
+                        .cornerRadius(15)
+                        .padding(.trailing)
+                        VStack(alignment: .leading) {
+                            Text(new.title).font(.headline).lineLimit(1)
+                            Text(new.subtitle).font(.subheadline).foregroundColor(.secondary).lineLimit(1)
+                            Text(new.body).font(.caption).foregroundColor(.secondary).lineLimit(2)
+                        }
+                        Spacer()
+                    }.padding(.bottom)
+                }
             }
-            #if os(macOS)
-            .padding()
-            .frame(width: 600, height: 600)
-            #endif
         }
     }
 
@@ -141,3 +184,8 @@ public struct Model: Codable, Hashable {
     var subtitle: String
     var body: String
 }
+
+//struct Previews: PreviewProvider {
+//    static var previews: some View {
+//    }
+//}
