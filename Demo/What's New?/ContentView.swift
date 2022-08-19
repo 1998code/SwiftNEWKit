8f21b6c44fea98362653064c4edc469af5814ac6
1998code <54872601+1998code@users.noreply.github.com>
2022-08-19 15:50:02 +0800

    Update Demo

--- Demo/What's New?/ContentView.swift
+++ Demo/What's New?/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 import SwiftNEW
 
 struct ContentView: View {
-    // MARK: - All States
+    
+    @State var preview: String = "Normal"
+    
+    // MARK: All States
     @State var showNew: Bool = false
     @State var align: HorizontalAlignment = .center
     @State var color: Color = .accentColor
@@ -17,25 +20,45 @@ struct ContentView: View {
     @State var labelColor: Color = Color(UIColor.systemBackground)
     @State var label: String = "Show Release Note"
     @State var labelImage: String = "arrow.up.circle.fill"
+    @State var history: Bool = true
+    @State var data: String = "data"
+    
     var body: some View {
         NavigationView {
-            // MARK: Choose either one size (Normal, Mini or invisible)
             
-            // MARK: Normal
-            SwiftNEW(show: $showNew, align: $align, color: $color, size: $size, labelColor: $labelColor, label: $label, labelImage: $labelImage)
+            // MARK: Choose either one size (Normal, Mini or Invisible)
             
-                .toolbar {
-                    ToolbarItemGroup(placement: .navigationBarLeading) {
-                        // MARK: Invisible
-                        SwiftNEW(show: $showNew, align: $align, color: $color, size: .constant("invisible"), labelColor: $labelColor, label: $label, labelImage: $labelImage)
-                    }
-                    ToolbarItemGroup(placement: .navigationBarTrailing) {
-                        // MARK: Mini
-                        SwiftNEW(show: $showNew, align: $align, color: $color, size: .constant("mini"), labelColor: $labelColor, label: $label, labelImage: $labelImage)
-                    }
+            TabView(selection: $preview) {
+                
+                // MARK: - Normal
+                SwiftNEW(show: $showNew, align: $align, color: $color, size: $size, labelColor: $labelColor, label: $label, labelImage: $labelImage, history: $history, data: $data)
+                
+                // Don't copy tabItem
+                    .tabItem { Text("Normal") }.tag("Normal")
+                
+                // MARK: - Mini (Toolbar only)
+                Text("Compatible with Toolbar")
+                    .tabItem { Text("Mini") }.tag("Mini")
+                
+                // MARK: - Invisible
+                SwiftNEW(show: $showNew, align: $align, color: $color, size: .constant("invisible"), labelColor: $labelColor, label: $label, labelImage: $labelImage, history: $history, data: $data)
                     
+                // Don't copy tabItem
+                    .tabItem { Text("Invisible") }.tag("Invisible")
+                
+                // MARK: - Remote Test (>3.0.0)
+                //         Firebase Real Time Database Demo
+                SwiftNEW(show: $showNew, align: $align, color: $color, size: $size, labelColor: $labelColor, label: $label, labelImage: .constant("icloud"), history: $history, data: .constant("https://testground-a937f-default-rtdb.firebaseio.com/.json?print=pretty"))
+                
+                // Don't copy tabItem
+                    .tabItem { Text("Remote") }.tag("Remote")
+                
+            }
+            .toolbar {
+                if preview == "Mini" {
+                    SwiftNEW(show: $showNew, align: $align, color: $color, size: .constant("mini"), labelColor: $labelColor, label: $label, labelImage: $labelImage, history: $history, data: $data)
                 }
-            
+            }
         }
         #if os(iOS)
         .navigationViewStyle(.stack)
