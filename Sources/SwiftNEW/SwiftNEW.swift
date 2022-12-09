345ca417905611f3d4f43602d12967b883ce8b15
Alberto Delle Donne <alberto.delledonne05@gmail.com>
2022-12-09 11:42:52 +0000

    Update SwiftNEW.swift

--- Sources/SwiftNEW/SwiftNEW.swift
+++ Sources/SwiftNEW/SwiftNEW.swift
@@ -286,6 +286,9 @@ struct AppIcon: View {
     var body: some View {
         Bundle.main.iconFileName
             .flatMap { UIImage(named: $0) }
-            .map { Image(uiImage: $0) }
+            .map { Image(uiImage: $0)
+                    .resizable()
+                    .frame(width: 128, height: 128)
+            }
     }
 }
